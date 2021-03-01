9ea3c39bfffd02a11a9acb2eef0c889d1993f701
yhusr <hao-118@qq.com>
2021-03-01 22:15:49 +0800

    自动化测试代码二次开发

--- Chapter13/src/main/java/com/course/cases/AddUserCase.java
+++ Chapter13/src/main/java/com/course/cases/AddUserCase.java
@@ -2,8 +2,16 @@ package com.course.cases;
 
 import com.course.config.TestConfig;
 import com.course.model.AddUser;
+import com.course.model.User;
 import com.course.utils.DatabaseUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -15,5 +23,33 @@ public class AddUserCase {
         AddUser addUser = sqlSession.selectOne("addUser");
         System.out.println(addUser);
         System.out.println(TestConfig.addUserUrl);
+
+        //发请求，获取结果
+        String result = getResult(addUser);
+        //验证返回结果
+        User user = sqlSession.selectOne("addUserTest",addUser);
+        System.out.println(user.toString());
+        Assert.assertEquals(result,addUser.getExpected());
+
+    }
+
+    private String getResult(AddUser addUser) throws IOException {
+        HttpPost post = new HttpPost(TestConfig.addUserUrl);
+        JSONObject param = new JSONObject();
+        param.put("userName",addUser.getUserName());
+        param.put("password",addUser.getPassword());
+        param.put("sex",addUser.getSex());
+        param.put("age",addUser.getAge());
+        param.put("permission",addUser.getPermission());
+        param.put("isDelete",addUser.getIsDelete());
+        //设置头信息
+        post.setHeader("content-type","application/json");
+        StringEntity entity = new StringEntity(param.toString(),"utf-8");
+        post.setEntity(entity);
+
+        //获取响应信息
+        HttpResponse response = TestConfig.httpClient.execute(post);
+        String result = EntityUtils.toString(response.getEntity(),"utf-8");
+        return result;
     }
 }
